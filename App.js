3bf55800b483cf697d8108c7d57e0da1a3df8928
rahul nath <rahul.nath@intoloindia.com>
2022-09-13 19:23:38 +0530

    app componet

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import AnimationWaterapp from './AnimationWaterapp';
+import AnimationWaterapp from './componets/AnimationWaterapp';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// const Tab = createBottomTabNavigator();
+
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={HomeScreen} />
+//       <Tab.Screen name="Settings" component={SettingsScreen} />
+//     </Tab.Navigator>
+//   );
+// }
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +23,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Water info" component={AnimationWaterapp}  />
+        {/* <MyTabs /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
