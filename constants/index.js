d885050e3f9d5cb99f783791aa94195c1951e91f
Intoloshubham <shubham.sahu@intoloindia.com>
2023-01-16 16:24:13 +0530

    1 16 2023 CMT 7TH

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,6 @@
 import theme, {COLORS, SIZES, FONTS} from './theme';
 import images from './images';
 import icons from './icons';
+import constant from './constant.js';
 
-export {theme, COLORS, SIZES, FONTS, images, icons};
+export {theme, COLORS, SIZES, FONTS, images, icons,constant};
